e8066b2bebf0c9773921e5bcd9f90e56ecda79f3
Nils Thamm <nils.thamm@student.hpi.de>
2019-11-07 17:07:00 +0100

    Dump

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -1,11 +1,20 @@
 package de.hpi.ddm.actors;
 
 import java.io.Serializable;
+import java.time.Duration;
+import java.util.concurrent.CompletionStage;
 
+import akka.Done;
+import akka.NotUsed;
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import akka.actor.Props;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+import akka.stream.OverflowStrategies;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -44,7 +53,9 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	/////////////////
 	// Actor State //
 	/////////////////
-	
+
+	private String test1 = "";
+
 	/////////////////////
 	// Actor Lifecycle //
 	/////////////////////
@@ -52,12 +63,13 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////
 	// Actor Behavior //
 	////////////////////
-	
+
 	@Override
 	public Receive createReceive() {
 		return receiveBuilder()
 				.match(LargeMessage.class, this::handle)
 				.match(BytesMessage.class, this::handle)
+				.match(Source.class, this::handle)
 				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
 				.build();
 	}
@@ -65,18 +77,24 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	private void handle(LargeMessage<?> message) {
 		ActorRef receiver = message.getReceiver();
 		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
-		
-		// This will definitely fail in a distributed setting if the serialized message is large!
-		// Solution options:
-		// 1. Serialize the object and send its bytes batch-wise (make sure to use artery's side channel then).
-		// 2. Serialize the object and send its bytes via Akka streaming.
-		// 3. Send the object via Akka's http client-server component.
-		// 4. Other ideas ...
-		receiverProxy.tell(new BytesMessage<>(message.getMessage(), this.sender(), message.getReceiver()), this.self());
+
+
+		// TODO: message.getMessage serialisieren ohne auf das Serializable interface zu setzen
+		// TODO: Stream Verbindung zum receiving LMProxy aufbauen und Stream mit serialisiertem Inhalt senden
+		Source<Integer, NotUsed> source = Source.range(1, 10);
+		receiverProxy.tell(source, this.self());
+		//receiverProxy.tell(new BytesMessage<>(message.getMessage(), this.sender(), message.getReceiver()), this.self());
 	}
 
 	private void handle(BytesMessage<?> message) {
-		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
+		// TODO: Wenn der stream aufgehört hat, dann Object deserialisierne und an Empfänger senden
 		message.getReceiver().tell(message.getBytes(), message.getSender());
+	}
+
+	private void handle(Source message) {
+		Materializer materializer = ActorMaterializer.create(this.context());
+		CompletionStage<Done> done = message.runForeach(i -> this.test1 += i, materializer);
+		done.thenRun(() -> System.out.println(this.test1));
+		System.out.println(this.self().path());
 	}
 }
