ce104ef46882284dd782df69156d5c2e9c3768a2
Ramin Gharib <ramingharib@gmail.com>
2020-01-09 09:22:06 +0100

    add debug mode and create dataframe with the key-value columns

--- assignment4/src/main/scala/com/datavaders/Sindy.scala
+++ assignment4/src/main/scala/com/datavaders/Sindy.scala
@@ -3,22 +3,21 @@ package com.datavaders
 import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.Row
 
 import scala.collection.mutable.ListBuffer
 
 object Sindy {
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
 
-
-
     //------------------------------------------------------------------------------------------------------------------
     // Loading data
     //------------------------------------------------------------------------------------------------------------------
 
-    // TODO: Make folder name a parameter
     // Read a Dataset from a file
     val tables: ListBuffer[DataFrame] = ListBuffer()
-    inputs.foreach( file_name => {
+    inputs.foreach(file_name => {
       tables += spark.read
         .option("inferSchema", "true")
         .option("header", "true")
@@ -26,18 +25,23 @@ object Sindy {
         .csv(file_name)
     })
 
+    // DEBUG
+    //    val inputs = List("artists", "tracks").map(name => s"./TPCH/$name.csv")
+    //    val tables: ListBuffer[DataFrame] = ListBuffer()
+    //    inputs.foreach(file_name => {
+    //      tables += spark.read
+    //        .option("inferSchema", "true")
+    //        .option("header", "true")
+    //        .option("delimiter", ",")
+    //        .csv(file_name)
+    //    })
 
-    val columns = spark.emptyDataFrame
     tables.foreach(table => {
-      table.schema.names.foreach( column_name => {
-        val df = spark.emptyDataFrame
-        df.withColumn("key", collect_set(table.col(column_name)))
-        size
-        df.withColumn("value", typedLit(List.fill(size(collect_set(table.col(column_name)))))(column_name))
-        columns.union(df)
-        collect_set(table.col(column_name))
+      table.schema.names.foreach(column_name => {
+        val keys = table.select(column_name).distinct().toDF("key")
+        val columns = keys.withColumn("value", lit(column_name)).toDF()
+        columns.show()
       })
     })
-
   }
 }
