2a2501ccd9a85f9b502736ccf44bbd144bf90add
Ramin Gharib <ramingharib@gmail.com>
2020-01-13 17:42:57 +0100

    refactor code

--- assignment4/src/main/scala/com/datavaders/Sindy.scala
+++ assignment4/src/main/scala/com/datavaders/Sindy.scala
@@ -1,12 +1,8 @@
 package com.datavaders
 
-import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.expressions._
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.Row
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
-import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 object Sindy {
@@ -18,25 +14,25 @@ object Sindy {
     //------------------------------------------------------------------------------------------------------------------
 
     // Read a Dataset from a file
+    //    val tables: ListBuffer[DataFrame] = ListBuffer()
+    //    inputs.foreach(file_name => {
+    //      tables += spark.read
+    //        .option("inferSchema", "true")
+    //        .option("header", "true")
+    //        .option("delimiter", ";")
+    //        .csv(file_name)
+    //    })
+
+    // DEBUG
+    val inputs = List("artists", "tracks").map(name => s"./TPCH/$name.csv")
     val tables: ListBuffer[DataFrame] = ListBuffer()
     inputs.foreach(file_name => {
       tables += spark.read
         .option("inferSchema", "true")
         .option("header", "true")
-        .option("delimiter", ";")
+        .option("delimiter", ",")
         .csv(file_name)
     })
-
-    // DEBUG
-//        val inputs = List("artists", "tracks").map(name => s"./TPCH/$name.csv")
-//        val tables: ListBuffer[DataFrame] = ListBuffer()
-//        inputs.foreach(file_name => {
-//          tables += spark.read
-//            .option("inferSchema", "true")
-//            .option("header", "true")
-//            .option("delimiter", ",")
-//            .csv(file_name)
-//        })
 
     // Produce flattened and unionized columns dataframe
     var flatenedColumns = spark.emptyDataFrame.withColumn("key", lit("")).withColumn("value", lit(""))
@@ -50,59 +46,48 @@ object Sindy {
 
     flatenedColumns.as[(String, String)]
       // Group data frame by data entries (key = data entry; value = column name) (Can't user show after that)
-//      .groupByKey{ case (key, value) => key}
       .groupBy(flatenedColumns.col("key"))
       .agg(collect_list(col("value")) as "value").select("value")
       // Remove duplicated column list entries
       .distinct()
-//      // For each row with column lists explode all values and add the original list as a second column to it
+      // For each row with column lists explode all values and add the original list as a second column to it
       .withColumn("value_exploded", explode($"value"))
       .as[(List[String], String)].sort("value_exploded")
       // Filter column name out of inclusion list (for some reason it didn't worked if we do it before the filtering)
       .map(row => (row._1.filterNot(x => x.equals(row._2)).sorted, row._2))
       // For each column we want to see the collected column lists (Can't user show after that)
-      .groupByKey { case (list, value) => value }
+      .groupByKey { case (_, value_exploded) => value_exploded }
       // For columns where we have an inclusion dependency return this list. For others return null
-      .mapGroups{ case(group, values) => {
-        val valuesList = values.toList
-        var returnValue : (List[String], String) = null
-        if(valuesList.nonEmpty) {
+      .mapGroups { case (group_name, inclusion_lists) =>
+        val valuesList = inclusion_lists.toList
+        var returnValue: (List[String], String) = null
+        if (valuesList.nonEmpty) {
           var valueSet = valuesList.head._1.toSet
           valuesList.foreach(row => {
             valueSet = valueSet.&(row._1.toSet)
           })
-          if(valueSet.toList.isEmpty) {
-            returnValue = (null, group)
+          if (valueSet.toList.isEmpty) {
+            returnValue = (null, group_name)
           } else {
-            returnValue = (valueSet.toList.sorted, group)
+            returnValue = (valueSet.toList.sorted, group_name)
           }
         } else {
-          returnValue = (null, group)
+          returnValue = (null, group_name)
         }
         returnValue
-      }}
+      }
       // Remove unsuitable candidates
       .filter(row => row._1 != null)
       // Sorting
       .sort("_2")
       // Print the output
       .collect()
-      .foreach( row => {
+      .foreach(row => {
         print(row._2 + "  < ")
         print(row._1.head)
-        row._1.splitAt(1)._2.foreach( string => print(", " + string))
+        row._1.splitAt(1)._2.foreach(string => print(", " + string))
         println()
       })
-//      .show(false)
-
-
-
-    //    println(spark.emptyDataFrame.withColumn("test", explode(($"value").as("value"))).show())
-//    reducedCols.mapGroups((key, valueIterator) => {
-//      var strings: ListBuffer[String] = ListBuffer()
-//      valueIterator.foreach(value => {
-//        strings += value.getString(0) })
-//      return strings
-//      }).show()
+    //      .show(false)
   }
 }
